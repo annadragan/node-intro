8d3b0a8d36d4c197949ff183cd4350c200acd20b
Anna Dragan <anulkadragan@gmail.com>
2022-03-15 10:36:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const [nodePath, filePath, firstNumberString, secondNumberString] = process.argv);
+
+// const firstNumber = Number(firstNumberString);
+// const secondNumber = Number(secondNumberString);
+
+// function add(firstNumber, secondNumber);
+
+// function add (a, b) {
+//   return a + b;
+// }
+
+// const result = add(3,2);
+// console.log(result);
